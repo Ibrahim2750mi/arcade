625430db7dbf98cd6df0bfe453397efd0aeb3ac7
Ibrahim2750mi <cshore.2750mi@gmail.com>
2022-04-28 11:39:12 +0530

    added reshaping

--- arcade/gui/constructs.py
+++ arcade/gui/constructs.py
@@ -11,7 +11,7 @@ from arcade.gui.events import UIOnClickEvent
 from arcade.gui.mixins import UIMouseFilterMixin
 from arcade.gui.widgets.buttons import UIFlatButton, UITextureButton
 from arcade.gui.widgets.layout import UIBoxLayout, UIAnchorLayout, UIGridLayout
-from arcade.gui.widgets.text import UITextArea
+from arcade.gui.widgets.text import UITextArea, UILabel
 
 
 class UIMessageBox(UIMouseFilterMixin, UIAnchorLayout):
@@ -106,9 +106,9 @@ class UIFIleChooser(UIGridLayout):
                 size_hint_min=size_hint_min,
                 size_hint_max=size_hint_max,
                 style=style,
-                column_count = 3,
+                column_count = 4,
                 row_count = 2,
-                horizontal_spacing = 0.2*600 - 100,
+                horizontal_spacing = 0.2*width - 100,
                 vertical_spacing = 10,
                 **kwargs)
 
@@ -119,21 +119,36 @@ class UIFIleChooser(UIGridLayout):
 
         self.with_background(texture=self._bg_tex)
 
-        select_button = UIFlatButton(text="SELECT", width=100)
-        select_button.on_click = self._select
-        self.add(select_button, 2, 0)
+        self.select_button = UIFlatButton(text="SELECT", width=width*0.15)
+        self.select_button.on_click = self._select
+        self.add(self.select_button, 3, 0)
 
         # quick places
-        self.quick_places = [Path.home(), ]
+        quick_place_label = UILabel(text="Quick Places")
+        home_button = UIFlatButton(text="Home")
+        home_button.on_click = partial(self._expand_dir, file_dir=Path.home())
+
+        self.add(quick_place_label, 0, 0)
+        self.add(home_button, 0, 1)
+
+        self._quick_place_widgets = [quick_place_label, home_button]
+
+        self.height_ = height
+        self.width_ = width
 
         self.setup()
         self.selected = False
 
     def _expand_dir(self, _: UIOnClickEvent, file_dir: Union[PosixPath, WindowsPath]):
         self._path = file_dir
+        print(file_dir)
         if file_dir.is_file():
             return
         self.clear()
+        self.add(self.select_button, 2, 0)
+        for i in range(2):
+            self.add(self._quick_place_widgets[i], 0, i)
+
         self.setup()
 
     def _select(self, _: UIOnClickEvent):
@@ -147,12 +162,26 @@ class UIFIleChooser(UIGridLayout):
 
     def setup(self):
         tex = load_texture(file_name=":resources:gui_basic_assets/icons/file_not_selected.png")
-        for row_index, file in enumerate(self._path.iterdir()):
-            if row_index >= self.row_count - 1:
-                self.row_count += 1
-            file_button = UITextureButton(texture=tex, width=220, height=40, text=file.name, style={"font_color": (0, 0, 0)})
-            file_button.on_click = partial(self._expand_dir, file_dir=file)
-            self.add(file_button, 1, row_index + 1)
+
+        # 40 is the height for UITextureButton. 10 is vertial spacing
+        reshape_size = int(len(tuple(self._path.iterdir()))/ (self.height_ // (40 + 10))) + 1
+        reshaped_dir = [[] for _ in range(reshape_size)]
+        file_index = 0
+        # reshaping the iterated dir
+        for file in self._path.iterdir():
+            if len(reshaped_dir[file_index]) >= self.height_ // (40 + 10):
+                file_index += 1
+            reshaped_dir[file_index].append(file)
+
+        for col_index, reshaped_dir in enumerate(reshaped_dir, start=1):
+            for row_index, file in enumerate(reshaped_dir, start=1):
+                if row_index >= self.row_count:
+                    self.row_count += 1
+                file_button = UITextureButton(texture=tex, width=0.25*self.width_, height=40, text=file.name, 
+                                              style={"font_color": (0, 0, 0)})
+                file_button.on_click = partial(self._expand_dir, file_dir=file)
+
+                self.add(file_button, col_index, row_index)
     
     @property
     def path(self):
